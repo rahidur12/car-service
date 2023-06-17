20e05faa734db825c0fa10598e356c49247308fc
Armanhozyn <armanhossen591@gmail.com>
2023-06-17 20:54:13 +0600

    hello

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -14,7 +14,7 @@ class BookingController extends Controller
         // $data = array("name"=> $request->name , "body" => "Customer Email: $request->email, Service: $request->service, Date: $request->date, Special Request: $request->special_request");
         $data =  "Customer Name:$request->name, Customer Email: $request->email, Service: $request->service, Date: $request->date, Special Request: $request->special_request";
         Mail::raw($data, function($message){
-        $message->to('rakibtac@gmail.com')
+        $message->to('coklateboy7@gmail.com')
         ->subject("New Booking Arrived!!");
         $message->from("no-reply@dxbcarrepair.com","This is test");
         });
