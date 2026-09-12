5e06658bcbbfc52d95bd8db07c08eb314d61272b
op <mdopi35@gmail.com>
2026-09-12 19:32:12 +0600

    google tag added

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -17,6 +17,14 @@
     gtag('config', 'G-0WGTVV66H6');
     </script>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MF8QMQXB');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Style Dependencies -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{asset('public/img/logo-round.png')}}" rel="icon" type="image/webp">
